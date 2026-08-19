d5857bb5d6e2271babeead780e31ac8a5d75a30e
Piyush <piyushth090@gmail.com>
2026-08-19 15:18:35 +0530

    Force IPv4 in nodemailer transporter to fix Render ENETUNREACH

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,7 +8,8 @@ const sendEmail = async (options) => {
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
-    }
+    },
+    family: 4 // Force IPv4 to prevent ENETUNREACH / connection timeout on Render
   });
 
   const mailOptions = {
